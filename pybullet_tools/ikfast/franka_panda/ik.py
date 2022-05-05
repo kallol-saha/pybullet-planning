de9bd38b36bc46bbecc91964fa8992fa0eacf2ea
anthonysimeonov <asimeono@mit.edu>
2022-05-05 13:40:46 -0400

    2f140 assets

--- pybullet_tools/ikfast/franka_panda/ik.py
+++ pybullet_tools/ikfast/franka_panda/ik.py
@@ -5,6 +5,7 @@ from ..ikfast import * # For legacy purposes
 #FRANKA_URDF = "models/franka_description/robots/panda_arm.urdf"
 #FRANKA_URDF = "models/franka_description/robots/hand.urdf"
 FRANKA_URDF = "models/franka_description/robots/panda_arm_hand.urdf"
+FRANKA_URDF_2F140 = "models/franka_description/robots/panda_2f140.urdf"
 
 PANDA_INFO = IKFastInfo(module_name='franka_panda.ikfast_panda_arm', base_link='panda_link0',
                         ee_link='panda_link8', free_joints=['panda_joint7'])
